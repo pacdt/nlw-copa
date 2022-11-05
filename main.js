b2f17c5921b78b323bb356bc4a7298e2ff5623a2
Mateus Santos <mateus15do2@gmail.com>
2022-11-04 23:54:21 -0300

    add new features

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-function createGame(player1, hour, player2) {
+function createGame(player1, hour, group, player2) {
   return `
   <li>
           <img src="./assets/flags/icon=${player1}.svg" alt="Bandeira do ${player1}" />
-          <strong>${hour}</strong>
+          <strong><span>grupo ${group}</span> ${hour}</strong>
           <img src="./assets/flags/icon=${player2}.svg" alt="Bandeira da ${player2}" />
   </li>
+
+  <p>
+  <span>
+  ${player1}</span>
+  <span>${player2}</span>
+  </p>
   `
 }
 let delay = -1;
@@ -18,20 +24,16 @@ function createCard(date, day, games) {
       </ul>
     </div>`
 }
-document.querySelector("#cards").innerHTML = 
-    createCard(
-      "24/11",
-      "quinta",
-      createGame("brazil","16:00","serbia")
-    ) +
-    createCard(
-      "28/11",
-      "segunda",
-      createGame("brazil", "13:00", "switzerland")
-      ) +
-    createCard(
-      "02/12",
-      "sexta",
-      createGame("cameroon", "16:00", "brazil") +
-      createGame("portugal", "13:00", "uruguay")
-    )
+document.querySelector("#cards").innerHTML =
+  createCard("24/11", "quinta", createGame("brazil", "16:00", "G", "serbia")) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("brazil", "13:00", "G", "switzerland")
+  ) +
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("cameroon", "16:00", "G", "brazil") +
+      createGame("portugal", "13:00", "G", "uruguay")
+  )
