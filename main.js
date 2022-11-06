d8ad0d2a5049d34ef50f3a30f81e735ce397800b
Mateus Santos <mateus15do2@gmail.com>
2022-11-06 15:52:45 -0300

    new: color red, icon, logo & stadium icon

--- main.js
+++ main.js
@@ -2,19 +2,31 @@ function createGame(player1, hour, group, stadium, player2) {
   return `
   <li>
     <main>
-
       <div>
       <img src="./assets/flags/icon-${player1}.svg" alt="Bandeira do ${player1}">
       <span>${player1.substr(0, 3)}</span>
       </div>
 
-      <strong><span>grupo ${group}</span> ${hour}<span class="stadium">${stadium}</span></strong>
+      <strong>
       
+      <span>grupo ${group}</span>
+
+      ${hour}
+
+        <div>
+          <span class="stadium">
+          <img src='./assets/stadium.svg' alt='stadium icon'>
+          ${stadium}
+          </span>
+        </div>
+
+       </strong>
+
       <div>
+
       <img src="./assets/flags/icon-${player2}.svg" alt="Bandeira da ${player2}" />
       <span>${player2.substr(0, 3)}</span>
       </div>
-
     </main>
   </li>
   
@@ -41,40 +53,46 @@ document.querySelector("#cards").innerHTML =
   createCard(
     "21/11",
     "segunda",
-    createGame("england", "10:00", "b", "khalifa", "iran")+
-    createGame('senegal','13:00','a','al-thumama','netherlands')+
-    createGame('united states', '16:00','b','al-rayyan','wales')
+    createGame("england", "10:00", "b", "khalifa", "iran") +
+      createGame("senegal", "13:00", "a", "al-thumama", "netherlands") +
+      createGame("united states", "16:00", "b", "al-rayyan", "wales")
   ) +
-  createCard('22/11', 'terça',
-  createGame('argentina', '07:00','c','lusail','saudi arabia')+
-  createGame('denmark', '10:00','d','Education City','tunisia')+
-  createGame('mexico', '13:00','c','ras abu aboud', 'poland')+
-  createGame('france','16:00','d','al-janoub','australia')
-  )+
   createCard(
-    '23/11','quarta',
-    createGame('morocco','07:00','f','al-bayt','croatia')+
-    createGame('germany','10:00','e','khalifa','japan')+
-    createGame('spain','13:00','e','al-thumama','costa rica')+
-    createGame('belgium','16:00','f','al-rayyan','canada')
+    "22/11",
+    "terça",
+    createGame("argentina", "07:00", "c", "lusail", "saudi arabia") +
+      createGame("denmark", "10:00", "d", "Education City", "tunisia") +
+      createGame("mexico", "13:00", "c", "ras abu aboud", "poland") +
+      createGame("france", "16:00", "d", "al-janoub", "australia")
+  ) +
+  createCard(
+    "23/11",
+    "quarta",
+    createGame("morocco", "07:00", "f", "al-bayt", "croatia") +
+      createGame("germany", "10:00", "e", "khalifa", "japan") +
+      createGame("spain", "13:00", "e", "al-thumama", "costa rica") +
+      createGame("belgium", "16:00", "f", "al-rayyan", "canada")
   )
-
 
 const color = document.querySelector(".theme")
 
 function changeColorGreen(color) {
-  document.body.classList.remove("theme", "yellow", "blue", "theme")
+  document.body.classList.remove("theme", "yellow", "blue",'red')
   document.body.classList.toggle("green")
 }
 function changeColorBlue(color) {
-  document.body.classList.remove("green", "yellow", "theme")
+  document.body.classList.remove("green", "yellow", "theme",'red')
   document.body.classList.toggle("blue")
 }
 function changeColorPurple(color) {
   document.body.classList.toggle("purple")
-  document.body.classList.remove("green", "blue", "yellow")
+  document.body.classList.remove("green", "blue", "yellow",'red')
 }
 function changeColorYellow(color) {
-  document.body.classList.remove("green", "blue", "theme")
+  document.body.classList.remove("green", "blue", "theme",'red')
   document.body.classList.toggle("yellow")
+}
+function changeColorRed(color) {
+  document.body.classList.remove("green", "blue", "theme",'yellow')
+  document.body.classList.toggle("red")
 }
