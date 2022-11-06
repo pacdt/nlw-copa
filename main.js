e052001f7fcb47291021a846f5ef56e819dcb61f
Mateus Santos <mateus15do2@gmail.com>
2022-11-06 14:55:05 -0300

    add estadio e nome do time

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-function createGame(player1, hour, group, player2) {
+function createGame(player1, hour, group, stadium, player2) {
   return `
   <li>
-    <img src="./assets/flags/icon=${player1}.svg" alt="Bandeira do ${player1}" />
-    <strong><span>grupo ${group}</span> ${hour}</strong>
-    <img src="./assets/flags/icon=${player2}.svg" alt="Bandeira da ${player2}" />
+    <main>
+
+      <div>
+      <img src="./assets/flags/icon=${player1}.svg" alt="Bandeira do ${player1}">
+      <span>${player1.substr(0, 3)}</span>
+      </div>
+
+      <strong><span>grupo ${group}</span> ${hour}<span class="stadium">${stadium}</span></strong>
+      
+      <div>
+      <img src="./assets/flags/icon=${player2}.svg" alt="Bandeira da ${player2}" />
+      <span>${player2.substr(0, 3)}</span>
+      </div>
+
+    </main>
   </li>
   
   `
@@ -22,36 +34,31 @@ function createCard(date, day, games) {
 
 document.querySelector("#cards").innerHTML =
   createCard(
-    "24/11",
-    "quinta",
-    createGame("switzerland", "07:00", "g", "cameroon") +
-      createGame("argentina", "10:00", "h", "south korea") +
-      createGame("portugal", "13:00", "h", "ghana") +
-      createGame("brazil", "16:00", "G", "serbia")
+    "20/11",
+    "domingo",
+    createGame("qatar", "13:00", "a", "al-bayt", "ecuador")
   ) +
   createCard(
-    "28/11",
+    "21/11",
     "segunda",
-    createGame("brazil", "13:00", "G", "switzerland")
+    createGame("england", "10:00", "b", "khalifa", "iran")+
+    createGame('senegal','13:00','a','al-thumama','netherlands')+
+    createGame('united states', '16:00','b','al-rayyan','wales')
   ) +
+  createCard('22/11', 'terça',
+  createGame('argentina', '07:00','c','lusail','saudi arabia')+
+  createGame('denmark', '10:00','d','Education City','tunisia')+
+  createGame('mexico', '13:00','c','ras abu aboud', 'poland')+
+  createGame('france','16:00','d','al-janoub','australia')
+  )+
   createCard(
-    "02/12",
-    "sexta",
-    createGame("cameroon", "16:00", "G", "brazil") +
-      createGame("portugal", "13:00", "G", "uruguay")
-  ) +
-  createCard(
-    "02/12",
-    "sexta",
-    createGame("cameroon", "16:00", "G", "brazil") +
-      createGame("argentina", "13:00", "G", "uruguay")
-  ) +
-  createCard(
-    "02/12",
-    "sexta",
-    createGame("cameroon", "16:00", "G", "brazil") +
-      createGame("portugal", "13:00", "G", "brazil")
+    '23/11','quarta',
+    createGame('morocco','07:00','f','al-bayt','croatia')+
+    createGame('germany','10:00','e','khalifa','japan')+
+    createGame('spain','13:00','e','al-thumama','costa rica')+
+    createGame('belgium','16:00','f','al-rayyan','canada')
   )
+
 
 const color = document.querySelector(".theme")
 
